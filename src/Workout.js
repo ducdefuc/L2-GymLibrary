706b72e666ac6542d8fce41ce1156ff61a6933af
DucAnh Pham <ducanh.p@outlook.com>
2023-09-23 23:05:52 +0200

    Jsdoc comments for workout.js

--- src/Workout.js
+++ src/Workout.js
@@ -1,9 +1,18 @@
 import { Exercise } from './Exercise.js'
 
+/**
+ * Represents a workout with a specific name and a collection of exercises.
+ */
 export class Workout {
 
   #exercises = []
 
+  /**
+ * Creates a new workout with a specified name.
+ *
+ * @param {string} name - The name of the workout.
+ * @throws {Error} Throws an error if the name is not a valid string.
+ */
   constructor(name) {
     if (typeof name !== 'string' || name.length === 0) {
       throw new Error('Name must be a string with minimum 1 character.')
@@ -12,7 +21,12 @@ export class Workout {
     this.name = name
   }
 
-  // Add an exercise to the workout.
+  /**
+   * Adds a new exercise to the workout.
+   *
+   * @param {Exercise} exercise - The exercise to be added.
+   * @throws {Error} Throws an error if the provided item is not an instance of Exercise.
+   */
   addExercise(exercise) {
     if (!(exercise instanceof Exercise)) {
       throw new Error('The provided item is not an instance of Exercise.')
@@ -21,7 +35,12 @@ export class Workout {
     this.#exercises.push(exercise)
   }
 
-  // Remove an exercise
+  /**
+   * Removes an exercise from the workout based on its name.
+   *
+   * @param {string} exerciseName - The name of the exercise to be removed.
+   * @throws {Error} Throws an error if the workout does not contain an exercise with the specified name.
+   */
   removeExercise(exerciseName) {
     if (!this.#exercises.some(exercise => exercise.name === exerciseName)) {
       throw new Error('The workout does not contain an exercise with that name.')
@@ -29,7 +48,13 @@ export class Workout {
     this.#exercises = this.#exercises.filter(exercise => exercise.name !== exerciseName)
   }
 
-  // Helper function to format warmup sets
+  /**
+   * Formats warmup sets of the given exercise for display.
+   *
+   * @private
+   * @param {Exercise} exercise - The exercise whose warmup sets need to be formatted.
+   * @returns {string} A formatted string of all warmup sets for the exercise.
+   */
   formatWarmupSets(exercise) {
     // Return a string of all the warmup sets for an exercise
     return exercise.sets
@@ -38,7 +63,13 @@ export class Workout {
       .join('\n') // Add a newline between each set
   }
 
-  // Helper function to format working sets
+  /**
+   * Formats working sets of the given exercise for display.
+   *
+   * @private
+   * @param {Exercise} exercise - The exercise whose working sets need to be formatted.
+   * @returns {string} A formatted string of all working sets for the exercise.
+   */
   formatWorkingSets(exercise) {
     // Return a string of all the working sets for an exercise
     return exercise.sets
@@ -47,7 +78,11 @@ export class Workout {
       .join('\n') // Add a newline between each set
   }
 
-  // Show the workout.
+  /**
+   * Generates a string representation of the workout with its name and exercises.
+   *
+   * @returns {string} A constructed string containing details of the workout and its exercises.
+   */
   showWorkout() {
     // for each exercise in the workout, format the warmup sets and working sets.
     const exerciseDetails = this.#exercises.map(exercise => {
