a03e25333cc7dfce31d70f6eeac36f9d06220ccc
DucAnh Pham <ducanh.p@outlook.com>
2023-09-25 16:26:29 +0200

    Finalized all tests. Added timer tests

---
+++ timerTest.js
@@ -0,0 +1,24 @@
+import { GymLibrary } from "./src/GymLibrary.js"
+
+const testLibrary = new GymLibrary()
+
+console.log('-------------TESTING TIMER-------------')
+testLibrary.startRestTimer(10,
+  () => {
+    console.log("Rest time is over!")
+  },
+  (remainingSeconds) => {
+    console.log(`Remaining time: ${remainingSeconds} seconds`)
+  }
+)
+
+setTimeout(() => {
+  console.log('Pausing the rest timer...')
+  testLibrary.pauseRestTimer()
+
+  setTimeout(() => {
+    console.log('Resuming the rest timer...')
+    testLibrary.resumeRestTimer() 
+  }, 3000)
+
+}, 3000)
